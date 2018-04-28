585fe552a17ed650645006a125549c5f20c7f1f3
Gavin Dinubilo <gavincodes@gmail.com>
2018-04-28 16:25:55 -0700

    Convert to a next/now app

--- index.js
+++ index.js
@@ -2,42 +2,52 @@ const express = require('express');
 const graphql = require('graphql').graphql;
 const graphqlHTTP = require('express-graphql');
 var bodyParser = require('body-parser');
+const next = require('next')
 
 const schema = require('./schema');
 
-const app = express();
-const PORT = 3001;
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.use('/graphiql', graphqlHTTP({
-  schema,
-  pretty: true,
-  graphiql: true,
-}));
+app.prepare()
+  .then(() => {
+    const server = express();
 
-// Configure the app to use bodyParser()
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+    server.use('/graphiql', graphqlHTTP({
+      schema,
+      pretty: true,
+      graphiql: true,
+    }));
 
-app.all('/graphql', (req, res) => {
-  let graphqlQuery = req.query.query;
-  if (req.body) {
-    graphqlQuery = req.body.query;
-  }
+    // Configure the server to use bodyParser()
+    server.use(bodyParser.urlencoded({
+      extended: true
+    }));
+    server.use(bodyParser.json());
 
-  if (!graphqlQuery) {
-    return res.status(500).send('You must provide a query');
-  }
+    server.all('/graphql', (req, res) => {
+      let graphqlQuery = req.query.query;
+      if (req.body) {
+        graphqlQuery = req.body.query;
+      }
 
-  return graphql(schema, graphqlQuery)
-    .then(response => response.data)
-    .then(data => res.json(data))
-    .catch(err => console.error(err)); // eslint-disable-line no-console
-});
+      if (!graphqlQuery) {
+        return res.status(500).send('You must provide a query');
+      }
 
-app.use(express.static('public'))
+      return graphql(schema, graphqlQuery)
+        .then(response => response.data)
+        .then(data => res.json(data))
+        .catch(err => console.error(err)); // eslint-disable-line no-console
+    });
 
-app.listen(PORT, () => {
-  console.log('Server running on port', PORT); // eslint-disable-line no-console
-});
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(PORT, () => {
+      console.log('Server running on port', PORT); // eslint-disable-line no-console
+    });
+  });
---
+++ pages/_document.js
@@ -0,0 +1,92 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+class AppWrapper extends Document {
+  static getInitialProps ({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
+
+  render () {
+    return (
+      <html>
+        <Head>
+          <title>Cool QL Cool</title>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <style>{`.hljs {
+  display: block;
+  overflow-x: auto;
+  padding: 0.5em;
+  background: #272822; color: #ddd;
+}
+
+.hljs-tag,
+.hljs-keyword,
+.hljs-selector-tag,
+.hljs-literal,
+.hljs-strong,
+.hljs-name {color: #f92672;}
+.hljs-code {
+  color: #66d9ef;
+}
+
+.hljs-class .hljs-title {
+  color: white;
+}
+
+.hljs-attribute,
+.hljs-symbol,
+.hljs-regexp,
+.hljs-link {
+  color: #bf79db;
+}
+
+.hljs-string,
+.hljs-bullet,
+.hljs-subst,
+.hljs-title,
+.hljs-section,
+.hljs-emphasis,
+.hljs-type,
+.hljs-built_in,
+.hljs-builtin-name,
+.hljs-selector-attr,
+.hljs-selector-pseudo,
+.hljs-addition,
+.hljs-variable,
+.hljs-template-tag,
+.hljs-template-variable {
+  color: #a6e22e;
+}
+
+.hljs-comment,
+.hljs-quote,
+.hljs-deletion,
+.hljs-meta {
+  color: #75715e;
+}
+
+.hljs-keyword,
+.hljs-selector-tag,
+.hljs-literal,
+.hljs-doctag,
+.hljs-title,
+.hljs-section,
+.hljs-type,
+.hljs-selector-id {
+  font-weight: bold;
+}
+.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}`}</style>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
+
+export default AppWrapper;
